2f41a17bc8e1bb332ef35ca110bea190f731ddbf
badien <badien2172004@gmail.com>
2024-04-22 21:31:15 +0700

    create Component

---
+++ Chat/component/index.js
@@ -0,0 +1,13 @@
+    import Navbar from "./Navbar/Navbar";
+    import Error from "./Error/Error";
+    import Friend from "./Friend/Friend";
+    import Filter from "./Filter/Filter";
+    import Loader from "./Loader/Loader";
+    import UserCard from "./UserCard/UserCard";
+    import Model from "./Model/Model";
+    
+    
+    
+    
+    
+    export{Navbar,Error,Filter,Friend,Loader,UserCard,Model}   ;
---
+++ Chat/src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Myapp = ({Component, pageProps}) => {
+    return  <Component {...pageProps}/>
+}
+export default Myapp
