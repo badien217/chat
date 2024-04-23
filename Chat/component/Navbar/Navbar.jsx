843181b53895f1a9b0b59f493fa3b7426391d607
badien <badien2172004@gmail.com>
2024-04-23 22:26:46 +0700

    NARBAR

--- Chat/component/Navbar/Navbar.jsx
+++ Chat/component/Navbar/Navbar.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
-
+import Style from "./Navbar.module.css";
+import { ChatAppContext } from '../../context/ChatAppContext';
+import {Model ,Error} from  "../index"
 
 const Navbar = () => {
+    const menuItems = [
+        {
+            menu : "All Users",
+            link : "alluser"
+        },
+        {
+            menu : "Chat",
+            link :"/"
+        },
+        {
+            menu : "Contract",
+            link : "/"
+        },
+        {
+            menu : "Setting",
+            link : "/"
+        },
+        {
+            menu : "faqs",
+            link : "/"
+        },
+    ]
     return
-     <div>
-
+     <div className={Style.Navbar}>
+        <div className = {Style.Navbar_box}></div>
     </div>
 }
 export default Navbar
