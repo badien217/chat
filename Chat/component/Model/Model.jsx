03f929973a535bd92829ac1d7010dc01c8a54a74
vubadien <badien2172004@gmail.com>
2024-04-25 13:05:32 +0700

    model

--- Chat/component/Model/Model.jsx
+++ Chat/component/Model/Model.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import Image from "next/image";
+import Style from "./Model.module.css"
+import images from "../../assets"
+import { ChatAppContext } from "../../context/ChatAppContext";
+import {Loader} from "../../Components/index"
+const Model = (
+  {openBox,
+    title,
+    head,
+    info,
+    smallInfo,
+    image,
+    functionName,address }
 
+) => {
+  const [name,setName] = useState("");
+  const [accountAddress,setAccountAddress] = useState("");
+  const {loading} = useContext(ChatAppContext);
 
-const Model = () => {
-    return
-     <div>
+  return (
+    <div className={Style.Model}>
+      <div className={Style.Model_box}>
+        <div className={Style.Model_box_left}>
+          <Image src ={image} alt="buddy" width={700} height={700}/>
+        </div>
+        <div className={Style.Model_box_right}>
+          <h1>{title}
+          <span>
+            {head}
+          </span>
+          </h1>
+          <p>{info}</p>
+          <small>{smallInfo}</small>
+          {loading==true?(
+            <Loader></Loader>
+          ):(
+            <div className={Style.Model_box_right_name}>
+            <div className={Style.Model_box_right_name_info}>
+              <Image src={images.username} alt="user" width={30} height={30}/>
+              <input type="text" placeholder="your name"
+              onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className={Style.Model_box_right_name_info}>
+              <Image src={images.account} alt="user" width={30} height={30}/>
+              <input type="text" placeholder={address || "Enter address.."}
+              onChange={(e) => setAccountAddress(e.target.value)}
+              />
+            </div>
+            <div className={Style.Model_box_right_name_btn}>
+              <button onClick={()=> functionName({name,accountAddress})}>
+                {" "}
+                <Image src={images.send} alt="send" width={30} height={30}/>
+                {" "}
+                Submit
+              </button>
+              <button onClick={()=> openBox(false)}>
+                {" "}
+                <Image src={images.close} alt="send" width={30} height={30}/>
+                {" "}
+                Cancle
+              </button>
+            </div>
+          </div>
+          )}
+          
+        </div>
+
+      </div>
 
     </div>
-}
-export default Model
+  );
+};
+
+export default Model;
